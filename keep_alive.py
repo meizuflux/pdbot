2d84c872c51c8a75ff4f59bf132385779efd3ca7
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2020-12-14 21:34:21 +0000

    only works with 1 word

--- keep_alive.py
+++ keep_alive.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
+from threading import Thread
+import random
+
+
+app = Flask('')
 
 @app.route('/')
-def index():
-  return render_template("index.html")
+def home():
+	return 'Im in!'
 
-app.run(host='0.0.0.0', port=8080)
+def run():
+  app.run(
+		host='0.0.0.0',
+		port=random.randint(2000,9000)
+	)
+
+def keep_alive():
+	'''
+	Creates and starts new thread that runs the function run.
+	'''
+	t = Thread(target=run)
+	t.start()
