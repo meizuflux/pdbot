62282a204ad0ba7ce27feed2fdea8d5638364ded
ppotatoo <rkawas731@protonmail.com>
2020-12-18 18:44:26 +0000

    everything works. Start of cogs.

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import discord
 import random
 intents = discord.Intents(messages=True, guilds=True)
 from discord.ext import commands
-#from dotenv import load_dotenv
 
+bot = commands.Bot(command_prefix="!")
 
 token = os.environ['DTOKEN']
-
-#load_dotenv()
-#TOKEN = os.getenv('TOKEN')
 from keep_alive import keep_alive
-
-bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+class MyHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=discord.Color.blurple(), description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
 
 @bot.command(name='createchannel')
 @commands.has_role('admin')
@@ -98,13 +101,17 @@ async def on_message(message):
 		else:
 			channel = bot.get_channel(788471476927332382)
 			creationtime = message.created_at
-			embed = discord.Embed(title='*INCOMEING MESSAGE*', description=f'{message.author} sent "{message.content}"')
+			embed = discord.Embed(title='*INCOMEING MESSAGE*', description=f'{message.author.mention} sent "{message.content}"')
 			embed.colour = 0xFFFFFF
 			embed.set_thumbnail(url=message.author.avatar_url)
-			embed.set_footer(text=f"Sent at {creationtime}")
+			embed.set_footer(text=f"Sent at {creationtime}	ID: {message.author.id}")
+			#embed.set_author(*, name=f'{message.author}', url=Embed.Empty, icon_url=f'{message.author.avatar_url}')
 			await channel.send(embed=embed)
 			return
 	if message.channel.id == int(789329010080743444):
+		await message.add_reaction('\N{UPWARDS BLACK ARROW}')
+		await message.add_reaction('\N{DOWNWARDS BLACK ARROW}')
+	if message.channel.id == int(788058309973901344):
 		await message.add_reaction('\N{UPWARDS BLACK ARROW}')
 		await message.add_reaction('\N{DOWNWARDS BLACK ARROW}')
 
@@ -118,10 +125,11 @@ async def on_command(ctx):
 	embed = discord.Embed(title='Command Use', description=f'{ctx.author} used `!{ctx.command}` in #{channelID.name} in {ctx.guild}')
 	embed.colour = 0xFFFFFF
 	embed.set_thumbnail(url=ctx.author.avatar_url)
-	embed.set_footer(text=f"Sent at {creationtime}")  
+	embed.set_footer(text=f"Sent at {creationtime}	ID: {ctx.author.id}")  
 
 	await channel.send(embed=embed)
 
 
+bot.help_command = MyHelpCommand()
 keep_alive()
 bot.run(token)
