95e169da6d4eb17025bb933fb3a1a3467ccae579
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2020-12-18 19:29:24 +0000

    start of cogs. fun.py works. broken for !exembed command though.

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,13 @@
 from discord.ext import commands
+import discord
 
-class CommandEvents(commands.cog):
+class fun(commands.Cog):
 	def __init_(self, bot):
 		self.bot = bot
+
+	@commands.command(name='repeat', aliases=['copy', 'mimic'])
+	async def do_repeat(self, ctx, *, our_input: str):
+		await ctx.send(our_input)
+
+def setup(bot):
+	bot.add_cog(fun(bot))
