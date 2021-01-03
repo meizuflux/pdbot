bdddbb8ed3ef68c374a033408c0dc2aa62686142
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-01-03 12:48:09 -0500

    testing custom prefixes for each server

--- cogs/testingjson.py
+++ cogs/testingjson.py
@@ -2,12 +2,13 @@ from discord.ext import commands
 import discord
 import json
 
+with open("data.json", "r") as f:
+		data = json.load(f)
 class testingjson(commands.Cog, command_attrs=dict(hidden=False)):
 	def __init__(self, bot):
 		self.bot = bot
 
-	with open("data.json", "r") as f:
-		data = json.load(f)
+	
 
 	@commands.command(name='cutetest')
 	async def cute(self, ctx, *, message):
@@ -21,12 +22,26 @@ class testingjson(commands.Cog, command_attrs=dict(hidden=False)):
 	@commands.command(name='prefix')
 	@commands.has_role('MANAGER')
 	async def prefix(self, ctx, args):
-		with open("data.json", "r") as f:
-			data = json.load(f)
 		data["prefix"] = args
 		with open("data.json", "w") as f:
 			json.dump(data, f, indent=4)
 		await ctx.send(f'Set prefix to ' +data['prefix'])
+
+
+	@commands.command(name='testprefix', hidden=True)
+	@commands.has_role('MANAGER')
+	async def testprefix(self, ctx, args):
+		tonk = ctx.guild.name
+		data[tonk] = args
+		with open("data.json", "w") as f:
+			json.dump(data, f, indent=4)
+		await ctx.send(f'Set prefix to ' +data[tonk]+' in this server')
+
+
+	@commands.command(name='guildid')
+	async def nametest(self, ctx):
+		await ctx.send(ctx.guild.id)
+		
 
 
 
