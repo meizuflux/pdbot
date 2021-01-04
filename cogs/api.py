bd155872b7ab3f408b02b6d28d4fc300e3b46263
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-01-04 21:40:40 +0000

    changes to format

--- cogs/api.py
+++ cogs/api.py
@@ -13,11 +13,11 @@ class api(commands.Cog, command_attrs=dict(hidden=False)):
 			#await ctx.send('It looks like you did not enter a number')
 		data = requests.get(f'https://new.scoresaber.com/api/player/{args}/full').json()
 		
-		embed = discord.Embed(title='Command Use')
+		embed = discord.Embed(title=f"{data['playerInfo']['playerName']}\'s Profile", url=f"https://new.scoresaber.com/u/{args}", description=f"Player Ranking: #{data['playerInfo']['rank']} \nCountry Ranking: {data['playerInfo']['country']} #{data['playerInfo']['countryRank']} \nPerformance Points: {data['playerInfo']['pp']}")
 		embed.color = 0x2f3136
 		embed.set_thumbnail(url=f"https://new.scoresaber.com{data['playerInfo']['avatar']}")
 		embed.set_footer(text=f"Sent at {ctx.message.created_at}")
-		embed.add_field(name=data['playerInfo']['playerName'], value=f"PP: {data['playerInfo']['pp']} \nRank: #{data['playerInfo']['rank']} \nCountry Rank: {data['playerInfo']['country']} #{data['playerInfo']['countryRank']}", inline=False)
+		embed.add_field(name='Score Stats', value=f"Play Count: {data['scoreStats']['totalPlayCount']} \nRanked Play Count: {data['scoreStats']['rankedPlayCount']} \nAverage Ranked Accuracy: {data['scoreStats']['averageRankedAccuracy']}%", inline=False)
 		embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
 		embed.set_footer(text=f'Powered by the ScoreSaber API')
 
