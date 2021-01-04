eb4f0ccc52dd4e0bae78bafa5ada5a0ec15df4f2
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-01-04 18:37:39 +0000

    added stuff and fixed stuff

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -7,6 +7,9 @@ class reactions(commands.Cog, command_attrs=dict(hidden=True)):
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
+		if self.bot.user in message.mentions:
+			await message.add_reaction('<:what:791007602745671701>')
+		await message.add_reaction('<:SantaGiggle:795701579578605599>')
 		if message.channel.id == int(789329010080743444):
 			await message.add_reaction('\N{UPWARDS BLACK ARROW}')
 			await message.add_reaction('\N{DOWNWARDS BLACK ARROW}')
