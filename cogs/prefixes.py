964a10284915e51bb1aed21e33f63a478165dcc1
ppotatoo <rkawas731@protonmail.com>
2021-01-08 13:41:05 +0000

    prefix stuff

--- cogs/prefixes.py
+++ cogs/prefixes.py
@@ -26,7 +26,8 @@ class prefixes(commands.Cog, command_attrs=dict(hidden=True)):
 		with open('prefixes.json', 'w') as f:
 			json.dump(prefixes, f, indent=4)
 
-	@commands.command(name='changeprefix')
+	@commands.command(name='prefix')
+	@commands.has_permissions(manage_guild=True)
 	async def prefix(self, ctx, prefix: str):
 		with open('prefixes.json', 'r') as f:
 			prefixes = json.load(f)
@@ -35,6 +36,7 @@ class prefixes(commands.Cog, command_attrs=dict(hidden=True)):
 
 		with open('prefixes.json', 'w') as f:
 			json.dump(prefixes, f, indent=4)
+		await ctx.send(f'Set prefix to ' +prefixes[str(ctx.guild.id)])
 
 def setup(bot):
 	bot.add_cog(prefixes(bot))
--- cogs/tracking.py
+++ cogs/tracking.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import json
 
 class tracking(commands.Cog, command_attrs=dict(hidden=True)):
 	def __init__(self, bot):
@@ -20,6 +21,11 @@ class tracking(commands.Cog, command_attrs=dict(hidden=True)):
 				embed.set_author(name=f'{message.author}', url='https://www.urbandictionary.com/define.php?term=Your%20mum%20gay', icon_url=f'{message.author.avatar_url}')
 				await channel.send(embed=embed)
 				return
+		if self.bot.user in message.mentions:
+			with open("prefixes.json", "r") as f:
+				prefix = json.load(f)
+			await message.add_reaction('<:what:791007602745671701>')
+			await message.channel.send(f"Hello <@{message.author.id}>, my prefix on this server is `{prefix[str(message.guild.id)]}`")
 
 def setup(bot):
 	bot.add_cog(tracking(bot))
