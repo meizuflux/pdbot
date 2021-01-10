fc1c62221205d0b4986935c11bf2ac8dda669913
ppotatoo <rkawas731@protonmail.com>
2021-01-10 23:30:00 +0000

    extra stuff

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,7 +5,8 @@ import io
 import traceback
 from contextlib import redirect_stdout
 import time
-
+import aiohttp
+from utils import http
 # to expose to the eval command
 import datetime
 from collections import Counter
@@ -36,7 +37,70 @@ class Admin(commands.Cog, command_attrs=dict(hidden=True)):
             return f'```py\n{e.__class__.__name__}: {e}\n```'
         return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
-    
+    @commands.group()
+    @commands.is_owner()
+    async def change(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(str(ctx.command))
+
+    @change.command(name="username")
+    @commands.is_owner()
+    async def change_username(self, ctx, *, name: str):
+        """ Change username. """
+        try:
+            await self.bot.user.edit(username=name)
+            await ctx.send(f"Successfully changed username to **{name}**")
+        except discord.HTTPException as err:
+            await ctx.send(err)
+
+    @change.command(name="username")
+    @commands.is_owner()
+    async def change_username(self, ctx, *, name: str):
+        """ Change username. """
+        try:
+            await self.bot.user.edit(username=name)
+            await ctx.send(f"Successfully changed username to **{name}**")
+        except discord.HTTPException as err:
+            await ctx.send(err)
+
+    @change.command(name="nickname")
+    @commands.is_owner()
+    async def change_nickname(self, ctx, *, name: str = None):
+        """ Change nickname. """
+        try:
+            await ctx.guild.me.edit(nick=name)
+            if name:
+                await ctx.send(f"Successfully changed nickname to **{name}**")
+            else:
+                await ctx.send("Successfully removed nickname")
+        except Exception as err:
+            await ctx.send(err)
+
+    @change.command(name="avatar")
+    @commands.is_owner()
+    async def change_avatar(self, ctx, url: str = None):
+        """ Change avatar. """
+        if url is None and len(ctx.message.attachments) == 1:
+            url = ctx.message.attachments[0].url
+        else:
+            url = url.strip('<>') if url else None
+
+        try:
+            bio = await http.get(url, res_method="read")
+            await self.bot.user.edit(avatar=bio)
+            await ctx.send(f"Successfully changed the avatar. Currently using:\n{url}")
+        except aiohttp.InvalidURL:
+            await ctx.send("The URL is invalid...")
+        except discord.InvalidArgument:
+            await ctx.send("This URL does not contain a useable image")
+        except discord.HTTPException as err:
+            await ctx.send(err)
+        except TypeError:
+            await ctx.send("You need to either provide an image URL or upload one with the command")
+
+
+
+			
     @commands.command(name='eval', hidden=False, help='owner only suckers')
     async def _eval(self, ctx, *, body: str):
         """Evaluates a code"""
