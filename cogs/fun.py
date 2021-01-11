dc9505879eada86515c8c129b0c0de7cddb1c9fa
ppotatoo <rkawas731@protonmail.com>
2021-01-11 13:40:54 +0000

    sel supreme command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,8 @@
 from discord.ext import commands
 import discord
 import os
-import urllib.parse
-from utils import argparser
+
+
 flipnotetoken = os.environ['tflipnote']
 
 class fun(commands.Cog):
@@ -48,39 +48,6 @@ class fun(commands.Cog):
 	async def r(self, ctx):
 		await ctx.send('I cannot believe it. I can NOT fucking believe it. I simply REFUSE to believe the absolute imcompetent, negligence, of actually not, for ANY of these categories whatsoever, not picking up FUCKING Rascal. This guy doesn\'t get props by anyone, on no one\'s social media radar whatsoever. Everyone\'s talking about like "oh Smurf, ya know, Smurf he\'s-- poor Smurf!" think about Rascal! He literally came into the league at the start of the year, was the BEST Mei. He revolutionized the way you play Echo, and set the guidelines for everyone else in the league for MONTHS! Or pretty much like half the season! And then he comes into the Countdown Cup and plays the Genji, that actually turns the SanFranciscoShockaroundandtheywintheseriesagainstthePhiladelphiaFusion! How is NO ONE, on this PLANET talking about Rascal as one of the most underrated players of the year! It\'s absolutely... HURTING MY SOUL!')
 
-	@commands.command()
-	async def supreme(self, ctx, *, text: commands.clean_content(fix_channel_mentions=True)):
-		""" Make a fake Supreme logo
-        Arguments:
-            --dark | Make the background to dark colour
-            --light | Make background to light and text to dark colour
-        """
-		parser = argparser.Arguments()
-		parser.add_argument('input', nargs="+", default=None)
-		parser.add_argument('-d', '--dark', action='store_true')
-		parser.add_argument('-l', '--light', action='store_true')
-
-		args, valid_check = parser.parse_args(text)
-		if not valid_check:
-		    return await ctx.send(args)
-
-		inputText = urllib.parse.quote(' '.join(args.input))
-		if len(inputText) > 500:
-		    return await ctx.send(f"**{ctx.author.name}**, the Supreme API is \\limited to 500 characters, sorry.")
-
-		darkorlight = ""
-		if args.dark:
-		    darkorlight = "dark=true"
-		if args.light:
-		    darkorlight = "light=true"
-		if args.dark and args.light:
-		    return await ctx.send(f"**{ctx.author.name}**, you can't define both --dark and --light, sorry..")
-
-		await self.api_img_creator(ctx, f"https://api.alexflipnote.dev/supreme?text={inputText}&{darkorlight}", "supreme.png", token=flipnotetoken)
-
-	
-
-	
 
 def setup(bot):
 	bot.add_cog(fun(bot))
