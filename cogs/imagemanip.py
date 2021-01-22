b2181e79527b023e3594662ba56803d579dab90e
ppotatoo <rkawas731@protonmail.com>
2021-01-22 18:12:45 +0000

    image comms

--- cogs/imagemanip.py
+++ cogs/imagemanip.py
@@ -3,9 +3,11 @@ import discord
 import polaroid
 import functools
 import typing
+import os
 from io import BytesIO
+flipnotetoken = os.environ['tflipnote']
 
-class image(commands.Cog):
+class Image(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
@@ -32,6 +34,20 @@ class image(commands.Cog):
 			embed.set_footer(text=f"Powered by Polaroid")
 			await ctx.send(embed=embed, file=file)
 
+	async def dagpi_image(self, url):
+		cs = self.bot.session
+		r = await cs.get(f'https://api.dagpi.xyz/image/{url}', headers={'Authorization': flipnotetoken})
+		io = BytesIO(await r.read())
+		f = discord.File(fp=io, filename='dagpi.png')
+		return f
+
+	async def alex_image(self, url):
+		cs = self.bot.session
+		r = await cs.get(f'https://api.alexflipnote.dev/{url}', headers={'Authorization': self.bot.config['API']['alex_token']})
+		io = BytesIO(await r.read())
+		f = discord.File(fp=io, filename='alex.png')
+		return f
+
 	@commands.command(help='Makes an image rainbowey')
 	async def rainbow(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
 		await self.manip(ctx, image, method='apply_gradient')
@@ -40,11 +56,43 @@ class image(commands.Cog):
 	async def wide(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
 		await self.manip(ctx, image, method='resize', method_args=(2000, 900, 1), text='ｗｉｄｅ')
 
+	@commands.command(help='Inverts an image')
+	async def invert(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='invert')
+
+	@commands.command(help='It\'s like looking in a mirror')
+	async def flip(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='fliph')
+
+	@commands.command(help='Blurs an image? Duh')
+	async def blur(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='box_blur')
+
+	@commands.command(help='cursed')
+	async def sobelh(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='sobel_horizontal')
+
+	@commands.command(help='cursed')
+	async def sobelv(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='sobel_vertical')
+
+	@commands.command(help='Decomposes the image')
+	async def decompose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='decompose_max')
+
+	@commands.command(help='Turns an image black and white')
+	async def grayscale(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='grayscale')
+		
+	@commands.command(help='Solarizes an image')
+	async def solarize(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
+		await self.manip(ctx, image, method='solarize')
+		
 	@commands.command(help='Rotates an image sideways')
 	async def sideways(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
 		await self.manip(ctx, image, method='rotate90')
 
-	@commands.command(help='Rotates an image sideways')
+	@commands.command(help='Rotates an image upsidedown', example='upsidedown person')
 	async def upsidedown(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
 		await self.manip(ctx, image, method='rotate180')
 
@@ -68,4 +116,4 @@ class image(commands.Cog):
 
 
 def setup(bot):
-	bot.add_cog(image(bot))
+	bot.add_cog(Image(bot))
