2f11d2e3c1c8fafd92101670d31fb89b19045638
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-01-24 14:51:26 +0000

    things

--- cogs/api.py
+++ cogs/api.py
@@ -16,20 +16,21 @@ class api(commands.Cog, command_attrs=dict(hidden=False)):
 		BattleTag = BattleTag.replace('#', '-')
 		if len(region) == 0:
 			region = 'us'
-		data = requests.get(f'https://ow-api.com/v1/stats/pc/{region}/{BattleTag}/profile')
-		if data.status_code == 400:
-			await ctx.send('Player not found')
-		else:
-			data = data.json()
-			if data['private'] == True:
-				await ctx.send(f"Profile is private. They are level {data['prestige']}{data['level']}.")
+		cs = self.bot.session
+		async with cs.get(f'https://ow-api.com/v1/stats/pc/{region}/{BattleTag}/profile') as data:
+			if data.status == 400:
+				await ctx.send('Player not found')
 			else:
-				embed = discord.Embed(title=data['name'], url=f'https://www.overbuff.com/players/pc/{BattleTag}?mode=competitive', color=0x2f3136, description=f"Level: {data['prestige']}{data['level']} \nEndorsement Level: {data['endorsement']} \nTotal Games Won: {data['gamesWon']}")
-				embed.set_thumbnail(url=data['icon'])
-				embed.add_field(name='Competitive Stats', value=f"Tank SR: {data['ratings'][0]['level']}sr \nDamage SR: {data['ratings'][1]['level']}sr \nSupport SR: {data['ratings'][2]['level']}sr")
-				embed.set_footer(text=f'Powered by https://ow-api.com')
+				data = data.json()
+				if data['private'] == True:
+					await ctx.send(f"Profile is private.")
+				else:
+					embed = discord.Embed(title=data['name'], url=f'https://www.overbuff.com/players/pc/{BattleTag}?mode=competitive', color=0x2f3136, description=f"Level: {data['prestige']}{data['level']} \nEndorsement Level: {data['endorsement']} \nTotal Games Won: {data['gamesWon']}")
+					embed.set_thumbnail(url=data['icon'])
+					embed.add_field(name='Competitive Stats', value=f"Tank SR: {data['ratings'][0]['level']}sr \nDamage SR: {data['ratings'][1]['level']}sr \nSupport SR: {data['ratings'][2]['level']}sr")
+					embed.set_footer(text=f'Powered by https://ow-api.com')
 
-				await ctx.send(embed=embed)
+					await ctx.send(embed=embed)
 				
 	@commands.command(name='dankmeme', help='Sends a Dank Meme 😎')
 	async def reddit(self, ctx):
