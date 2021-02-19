dd2898a583c57acf5d38ebfd6577868e0c389b61
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-19 17:01:41 +0000

    rollback

--- utils/embed.py
+++ utils/embed.py
@@ -3,5 +3,5 @@ import discord
 async def send(ctx, text):
     bot = ctx.bot
     embed=discord.Embed(description=text[:2048], color=bot.embed_color, timestamp=ctx.message.created_at).set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed, mention=True)
+    await ctx.send(embed=embed)
 	
